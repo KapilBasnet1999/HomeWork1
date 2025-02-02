1dcc8f12e50563d8adad4ace7a4751b1b4e004ba
KAPIL BASNET <163415277+KapilBasnet1999@users.noreply.github.com>
2025-02-01 21:10:04 -0600

    Add files via upload

---
+++ HomeWork1.cs
@@ -0,0 +1,175 @@
+﻿// Written by Kapil
+// Feb 1 2025
+
+using System;
+
+namespace HomeWork1
+{
+    public class HomeWork1
+    {
+        // Class variables or member variables
+        private double baseLength = 0.0;
+        private double height = 0.0;
+        private int chocolateEggs = -1;
+        private int provinces = -1;
+        private int duchies = -1;
+        private int estates = -1;
+        private int targetRow = -1;
+        private int targetColumn = -1;
+        private int number = -1;
+
+        // Gets and Sets
+
+        public double BaseLength
+        {
+            get { return this.baseLength; }
+            set { this.baseLength = value; }
+        }
+
+        public double Height
+        {
+            get { return this.height; }
+            set { this.height = value; }
+        }
+
+        public int ChocolateEggs
+        {
+            get { return this.chocolateEggs; }
+            set { this.chocolateEggs = value; }
+        }
+
+        public int Provinces
+        {
+            get { return this.provinces; }
+            set { this.provinces = value; }
+        }
+
+        public int Duchies
+        {
+            get { return this.duchies; }
+            set { this.duchies = value; }
+        }
+
+        public int Estates
+        {
+            get { return this.estates; }
+            set { this.estates = value; }
+        }
+
+        public int TargetRow
+        {
+            get { return this.targetRow; }
+            set { this.targetRow = value; }
+        }
+
+        public int TargetColumn
+        {
+            get { return this.targetColumn; }
+            set { this.targetColumn = value; }
+        }
+
+        public int Number
+        {
+            get { return this.number; }
+            set { this.number = value; }
+        }
+
+        // Constructors
+
+        public HomeWork1() : this(0.0, 0.0, -1, -1, -1, -1, -1, -1, -1)
+        {
+            // Empty constructor
+        }
+
+        public HomeWork1(double aBaseLength, double aHeight) : this(aBaseLength, aHeight, -1, -1, -1, -1, -1, -1, -1)
+        {
+            // Constructor for Triangle Farmer
+        }
+
+        public HomeWork1(int aChocolateEggs) : this(0.0, 0.0, aChocolateEggs, -1, -1, -1, -1, -1, -1)
+        {
+            // Constructor for Four Sisters and the Duckbear
+        }
+
+        public HomeWork1(int aProvinces, int aDuchies, int aEstates) : this(0.0, 0.0, -1, aProvinces, aDuchies, aEstates, -1, -1, -1)
+        {
+            // Constructor for Dominion of Kings
+        }
+
+        public HomeWork1(int aTargetRow, int aTargetColumn) : this(0.0, 0.0, -1, -1, -1, -1, aTargetRow, aTargetColumn, -1)
+        {
+            // Constructor for Defense of Consolas
+        }
+
+        /*public HomeWork1(int aNumber) : this(0.0, 0.0, -1, -1, -1, -1, -1, -1, aNumber)
+        {
+            // Constructor for Repairing the Clocktower
+        }*/
+
+        public HomeWork1(double aBaseLength, double aHeight, int aChocolateEggs, int aProvinces, int aDuchies, int aEstates, int aTargetRow, int aTargetColumn, int aNumber)
+        {
+            // Full constructor
+            this.BaseLength = aBaseLength;
+            this.Height = aHeight;
+            this.ChocolateEggs = aChocolateEggs;
+            this.Provinces = aProvinces;
+            this.Duchies = aDuchies;
+            this.Estates = aEstates;
+            this.TargetRow = aTargetRow;
+            this.TargetColumn = aTargetColumn;
+            this.Number = aNumber;
+        }
+
+        // Methods
+
+        public double GetTriangleArea()
+        {
+            return (this.BaseLength * this.Height) / 2;
+        }
+
+        public int GetSistersEggs()
+        {
+            return this.ChocolateEggs / 4;
+        }
+
+        public int GetDuckbearEggs()
+        {
+            return this.ChocolateEggs % 4;
+        }
+
+        public int GetTotalScore()
+        {
+            return (this.Provinces * 6) + (this.Duchies * 3) + (this.Estates * 1);
+        }
+
+        public string GetDeploymentLocations()
+        {
+            return $"Deploy to:\n" +
+                   $"({this.TargetRow}, {this.TargetColumn - 1})\n" + // Left
+                   $"({this.TargetRow - 1}, {this.TargetColumn})\n" + // Up
+                   $"({this.TargetRow}, {this.TargetColumn + 1})\n" + // Right
+                   $"({this.TargetRow + 1}, {this.TargetColumn})";    // Down
+        }
+
+        public string GetTickOrTock()
+        {
+            return (this.Number % 2 == 0) ? "Tick" : "Tock";
+        }
+
+        // Override ToString method
+        public override string ToString()
+        {
+            string message = "";
+            message += "Base Length: " + this.BaseLength + "\n";
+            message += "Height: " + this.Height + "\n";
+            message += "Eggs: " + this.ChocolateEggs + "\n";
+            message += "Provinces: " + this.Provinces + "\n";
+            message += "Duchies: " + this.Duchies + "\n";
+            message += "Estates: " + this.Estates + "\n";
+            message += "Target Row: " + this.TargetRow + "\n";
+            message += "Target Column: " + this.TargetColumn + "\n";
+            message += "Number: " + this.Number + "\n";
+            return message;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,93 @@
+﻿// Written by Kapil
+// Feb 1 2025
+
+namespace HomeWork1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an instance of the Homework1 class
+            HomeWork1 homework = new HomeWork1();
+
+            Console.WriteLine("Choose a program to run:");
+            Console.WriteLine("1. The Triangle Farmer");
+            Console.WriteLine("2. The Four Sisters and the Duckbear");
+            Console.WriteLine("3. The Dominion of Kings");
+            Console.WriteLine("4. The Defense of Consolas");
+            Console.WriteLine("5. Repairing the Clocktower");
+            Console.Write("Enter your choice (1-5): ");
+
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    // Triangle Farmer
+                    Console.Write("Enter the base of the triangle: ");
+                    double baseLength = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("Enter the height of the triangle: ");
+                    double height = Convert.ToDouble(Console.ReadLine());
+
+                    homework.BaseLength = baseLength;
+                    homework.Height = height;
+                    Console.WriteLine($"The area of the triangle is: {homework.GetTriangleArea()}");
+                    break;
+
+                case 2:
+                    // Four Sisters and the Duckbear
+                    Console.Write("Enter the number of chocolate eggs: ");
+                    int eggs = Convert.ToInt32(Console.ReadLine());
+
+                    homework.ChocolateEggs = eggs;
+                    Console.WriteLine($"Each sister gets {homework.GetSistersEggs()} chocolate eggs.");
+                    Console.WriteLine($"The duckbear gets {homework.GetDuckbearEggs()} chocolate eggs.");
+                    break;
+
+                case 3:
+                    // Dominion of Kings
+                    Console.Write("Enter the number of provinces: ");
+                    int provinces = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Enter the number of duchies: ");
+                    int duchies = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Enter the number of estates: ");
+                    int estates = Convert.ToInt32(Console.ReadLine());
+
+                    homework.Provinces = provinces;
+                    homework.Duchies = duchies;
+                    homework.Estates = estates;
+                    Console.WriteLine($"Your total score is: {homework.GetTotalScore()}");
+                    break;
+
+                case 4:
+                    // Defense of Consolas
+                    Console.Write("Target Row? ");
+                    int targetRow = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Target Column? ");
+                    int targetColumn = Convert.ToInt32(Console.ReadLine());
+
+                    homework.TargetRow = targetRow;
+                    homework.TargetColumn = targetColumn;
+                    Console.WriteLine(homework.GetDeploymentLocations());
+
+                    // Play a sound
+                    Console.Beep();
+                    break;
+
+                case 5:
+                    // Repairing the Clocktower
+                    Console.Write("Enter a number: ");
+                    int number = Convert.ToInt32(Console.ReadLine());
+
+                    homework.Number = number;
+                    Console.WriteLine(homework.GetTickOrTock());
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid choice. Please select a number between 1 and 5.");
+                    break;
+            }
+
+        }
+    }
+}
